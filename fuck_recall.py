3c6266f5353134dcff12988f7f5b27b70f22bb4a
winters zhang <winters@wintersSever.linode.com>
2017-03-30 20:50:29 +0900

    函数分离

--- fuck_recall.py
+++ fuck_recall.py
@@ -42,13 +42,7 @@ def getPicUrl(file_name):
     print 'pic_url ------>', pic_url
     return pic_url
 
-# 从msg中获取位置信息
-# def getLocation(msg):
-#     x, y, location = re.search("<location x=\"(.*?)\" y=\"(.*?)\".*label=\"(.*?)\".*",
-#                                msg['OriContent']).group(1, 2, 3)
-#     return x.__str__(), y.__str__()
-
-
+# 从接受的信息中获取必要的字段，处理后返回信息字典
 def getSavingMsg(msg, msgType):
     mytime = time.localtime()  # 这儿获取的是本地时间
     # 获取用于展示给用户看的时间 2017/03/03 13:23:53
@@ -120,58 +114,38 @@ def getSavingMsg(msg, msgType):
             "msg_url": msg_url
         }
     else:
-        pass
+        raise Exception('传入的参数msgType不符合要求!')
 
     return saving_msg
 
+# 将已撤回的消息发给文件传输助手
+def SendRecalledMsg(old_msg):
+    # print(old_msg_id, old_msg)
+    # 如果是群消息
+    if not old_msg:
+        raise Exception('撤回的消息不在本地暂存的消息列表中!')
+    elif old_msg.get('msg_group'):
+        group_text = u'从群【' + old_msg['msg_group'] + u'】中'
+    else:
+        group_text = u''
 
-# 将接收到的消息存放在字典中，当接收到新消息时对字典中超时的消息进行清理
-# 没有注册note（通知类）消息，通知类消息一般为：红包 转账 消息撤回提醒等，不具有撤回功能
-@itchat.msg_register([TEXT, PICTURE, MAP, CARD, SHARING, RECORDING, ATTACHMENT, VIDEO, FRIENDS],
-                     isFriendChat=True)
-def SaveFriendsMsg(msg):
-    print 'receive message'
-    msg_id = msg['MsgId']  # 消息ID
+    msg_send = u"您的好友：" \
+               + old_msg.get('msg_from', '') \
+               + u"  在 [" + old_msg.get('msg_time_touser', '') \
+               + u"], " + group_text + u"撤回了一条 [" + old_msg['msg_type'] + u"] 消息, 内容如下:" \
+               + old_msg.get('msg_content', '')
 
-    saving_msg = getSavingMsg(msg, 'friendChat')
-    # 更新字典
-    # {msg_id:(msg_from,msg_time,msg_time_touser,msg_type,msg_content,msg_url)}
-    msg_dict.update({msg_id: saving_msg})
-    # 清理字典
-    ClearTimeOutMsg()
-
-#
-@itchat.msg_register([TEXT, PICTURE, MAP, CARD, SHARING, RECORDING, ATTACHMENT, VIDEO, FRIENDS],
-                     isGroupChat=True)
-def SaveGroupsMsg(msg):
-    print 'receive message'
-    msg_id = msg['MsgId']  # 消息ID
-
-    saving_msg = getSavingMsg(msg, 'groupChat')
-    # 更新字典
-    # {msg_id:(msg_group, msg_from,msg_time,msg_time_touser,msg_type,msg_content,msg_url)}
-    msg_dict.update({msg_id: saving_msg})
-    # 清理字典
-    ClearTimeOutMsg()
-
-#收到note类消息，判断是不是撤回并进行相应操作
-@itchat.msg_register([NOTE], isFriendChat=True, isGroupChat=True)
-def SendRecallMsg(msg):
-    # print(msg)
-    print msg['Text']
-    # 创建可下载消息内容的存放文件夹，并将暂存在当前目录的文件移动到该文件中
-    if not os.path.exists("./static/"):
-        os.mkdir("./static/")
-
-    # if re.search(r"\<replacemsg\>\<\!\[CDATA\[.*撤回了一条消息\]\]\>\<\/replacemsg\>", msg['Content']) != None:
-    if re.search(u".*撤回了一条消息", msg['Text']) != None:
-        if not re.search(u";msgid&gt;(.*?)&lt;/msgid", msg['Content']):
-            old_msg_id = re.search(u"\<msgid\>(.*?)\<\/msgid\>", msg['Content']).group(1)
-        else:
-            old_msg_id = re.search(u";msgid&gt;(.*?)&lt;/msgid", msg['Content']).group(1)
-
-        old_msg = msg_dict.get(old_msg_id, {})
-        #print(old_msg_id, old_msg)
+    if old_msg['msg_type'] == "Sharing":
+        msg_send += u", 链接: " + old_msg.get('msg_url', '')
+    elif old_msg['msg_type'] == 'Picture':
+        shutil.move(old_msg['msg_content'], r"./static/")
+        pic_url = getPicUrl(old_msg['msg_content'])
+        msg_send += ' ' + pic_url
+    elif old_msg['msg_type'] == 'Recording' \
+            or old_msg['msg_type'] == 'Video' \
+            or old_msg['msg_type'] == 'Attachment':
+        msg_send += u", 存储在当前目录下Revocation文件夹中"
+        shutil.move(old_msg['msg_content'], r"./static/")
         if old_msg.get('msg_group'):
             group_text = u'从群【' + old_msg['msg_group'] + u'】中'
         else:
@@ -197,6 +171,60 @@ def SendRecallMsg(msg):
         print 'msg_send: ---------->', msg_send
         itchat.send(msg_send, toUserName='filehelper') #将撤回消息的通知以及细节发送到文件助手
 
+    print 'msg_send: ---------->', msg_send
+    itchat.send(msg_send, toUserName='filehelper')  # 将撤回消息的通知以及细节发送到文件助手
+
+
+# 将接收到的消息存放在字典中，当接收到新消息时对字典中超时的消息进行清理
+# 没有注册note（通知类）消息，通知类消息一般为：红包 转账 消息撤回提醒等，不具有撤回功能
+# 处理朋友消息
+@itchat.msg_register([TEXT, PICTURE, MAP, CARD, SHARING, RECORDING, ATTACHMENT, VIDEO, FRIENDS],
+                     isFriendChat=True)
+def SaveFriendsMsg(msg):
+    print 'receive message'
+    msg_id = msg['MsgId']  # 消息ID
+
+    saving_msg = getSavingMsg(msg, 'friendChat')
+    # 更新字典
+    # {msg_id:(msg_from,msg_time,msg_time_touser,msg_type,msg_content,msg_url)}
+    msg_dict.update({msg_id: saving_msg})
+    # 清理字典
+    ClearTimeOutMsg()
+
+# 处理群消息
+@itchat.msg_register([TEXT, PICTURE, MAP, CARD, SHARING, RECORDING, ATTACHMENT, VIDEO, FRIENDS],
+                     isGroupChat=True)
+def SaveGroupsMsg(msg):
+    print 'receive message'
+    msg_id = msg['MsgId']  # 消息ID
+
+    saving_msg = getSavingMsg(msg, 'groupChat')
+    # 更新字典
+    # {msg_id:(msg_group,msg_from,msg_time,msg_time_touser,msg_type,msg_content,msg_url)}
+    msg_dict.update({msg_id: saving_msg})
+    # 清理字典
+    ClearTimeOutMsg()
+
+# 收到note类消息，判断是不是撤回并进行相应操作
+@itchat.msg_register([NOTE], isFriendChat=True, isGroupChat=True)
+def RecalledMsg(msg):
+    # print(msg)
+    print msg['Text']
+    # 创建可下载消息内容的存放文件夹，并将暂存在当前目录的文件移动到该文件中
+    if not os.path.exists("./static/"):
+        os.mkdir("./static/")
+
+    # if re.search(r"\<replacemsg\>\<\!\[CDATA\[.*撤回了一条消息\]\]\>\<\/replacemsg\>", msg['Content']) != None:
+    if re.search(u".*撤回了一条消息", msg['Text']) != None:
+        if not re.search(u";msgid&gt;(.*?)&lt;/msgid", msg['Content']):
+            old_msg_id = re.search(u"\<msgid\>(.*?)\<\/msgid\>", msg['Content']).group(1)
+        else:
+            old_msg_id = re.search(u";msgid&gt;(.*?)&lt;/msgid", msg['Content']).group(1)
+
+        # 从暂存的消息列表中获取撤回的消息
+        old_msg = msg_dict.get(old_msg_id)
+        # 将撤回的消息发给自己
+        SendRecalledMsg(old_msg)
         msg_dict.pop(old_msg_id)
         ClearTimeOutMsg()
 
