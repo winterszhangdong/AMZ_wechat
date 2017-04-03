8a9a49c388b6dbcc2dee2feb2ebeff08087f926d
winters zhang <winters@wintersSever.linode.com>
2017-04-03 23:07:10 +0900

    规范代码风格

--- fuck_recall.py
+++ fuck_recall.py
@@ -1,4 +1,4 @@
-#!usr/bin/env python
+#!usr/bin/python2
 # -*- coding:utf-8 -*-
 
 import sys
@@ -26,10 +26,10 @@ def ClearTimeOutMsg():
                 item = msg_dict.pop(msgid)
                 #print("超时的消息：", item['msg_content'])
                 #可下载类消息，并删除相关文件
-                if item['msg_type'] == "Picture" \
-                        or item['msg_type'] == "Recording" \
-                        or item['msg_type'] == "Video" \
-                        or item['msg_type'] == "Attachment":
+                if (item['msg_type'] == "Picture" or
+                    item['msg_type'] == "Recording" or
+                    item['msg_type'] == "Video" or
+                    item['msg_type'] == "Attachment"):
                     print("要删除的文件：", item['msg_content'])
                     os.remove(item['msg_content'])
 
@@ -46,12 +46,12 @@ def getPicUrl(file_name):
 def getSavingMsg(msg, msgType):
     mytime = time.localtime()  # 这儿获取的是本地时间
     # 获取用于展示给用户看的时间 2017/03/03 13:23:53
-    msg_time_touser = mytime.tm_year.__str__() \
-                      + "/" + mytime.tm_mon.__str__() \
-                      + "/" + mytime.tm_mday.__str__() \
-                      + " " + mytime.tm_hour.__str__() \
-                      + ":" + mytime.tm_min.__str__() \
-                      + ":" + mytime.tm_sec.__str__()
+    msg_time_touser = (mytime.tm_year.__str__()
+                      + "/" + mytime.tm_mon.__str__()
+                      + "/" + mytime.tm_mday.__str__()
+                      + " " + mytime.tm_hour.__str__()
+                      + ":" + mytime.tm_min.__str__()
+                      + ":" + mytime.tm_sec.__str__())
     msg_time = msg['CreateTime']  # 消息时间
 
     msg_type = msg['Type']  # 消息类型
@@ -129,11 +129,11 @@ def SendRecalledMsg(old_msg):
     else:
         group_text = u''
 
-    msg_send = u"您的好友：" \
-               + old_msg.get('msg_from', '') \
-               + u"  在 [" + old_msg.get('msg_time_touser', '') \
-               + u"], " + group_text + u"撤回了一条 [" + old_msg['msg_type'] + u"] 消息, 内容如下:" \
-               + old_msg.get('msg_content', '')
+    msg_send = (u"您的好友："
+               + old_msg.get('msg_from', '')
+               + u"  在 [" + old_msg.get('msg_time_touser', '')
+               + u"], " + group_text + u"撤回了一条 [" + old_msg['msg_type'] + u"] 消息, 内容如下:"
+               + old_msg.get('msg_content', ''))
 
     if old_msg['msg_type'] == "Sharing":
         msg_send += u", 链接: " + old_msg.get('msg_url', '')
@@ -141,9 +141,9 @@ def SendRecalledMsg(old_msg):
         shutil.move(old_msg['msg_content'], r"./static/")
         pic_url = getPicUrl(old_msg['msg_content'])
         msg_send += ' ' + pic_url
-    elif old_msg['msg_type'] == 'Recording' \
-            or old_msg['msg_type'] == 'Video' \
-            or old_msg['msg_type'] == 'Attachment':
+    elif (old_msg['msg_type'] == 'Recording' or
+          old_msg['msg_type'] == 'Video' or
+          old_msg['msg_type'] == 'Attachment'):
         msg_send += u", 存储在当前目录下Revocation文件夹中"
         shutil.move(old_msg['msg_content'], r"./static/")
         if old_msg.get('msg_group'):
@@ -151,20 +151,20 @@ def SendRecalledMsg(old_msg):
         else:
             group_text = u''
 
-        msg_send = u"您的好友：" \
-                   + old_msg.get('msg_from', '') \
-                   + u"  在 [" + old_msg.get('msg_time_touser', '') \
-                   + u"], " + group_text + u"撤回了一条 ["+old_msg['msg_type'] + u"] 消息, 内容如下:" \
-                   + old_msg.get('msg_content', '')
+        msg_send = (u"您的好友：" +
+                    old_msg.get('msg_from', '') +
+                    u"  在 [" + old_msg.get('msg_time_touser', '') +
+                    u"], " + group_text + u"撤回了一条 ["+old_msg['msg_type'] + u"] 消息, 内容如下:" +
+                    old_msg.get('msg_content', ''))
         if old_msg['msg_type'] == "Sharing":
             msg_send += u", 链接: " + old_msg.get('msg_url', '')
         elif old_msg['msg_type'] == 'Picture':
             shutil.move(old_msg['msg_content'], r"./static/")
             pic_url = getPicUrl(old_msg['msg_content'])
             msg_send += ' ' + pic_url
-        elif old_msg['msg_type'] == 'Recording' \
-                or old_msg['msg_type'] == 'Video' \
-                or old_msg['msg_type'] == 'Attachment':
+        elif (old_msg['msg_type'] == 'Recording' or
+              old_msg['msg_type'] == 'Video' or
+              old_msg['msg_type'] == 'Attachment'):
             msg_send += u", 存储在当前目录下Revocation文件夹中"
             shutil.move(old_msg['msg_content'], r"./static/")
 
